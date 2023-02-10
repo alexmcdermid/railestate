aa7e9d2f528c0725def2bc03048a66523a533fe9
alexmcdermid <alexander.mcdermid@gmail.com>
2023-02-10 11:43:29 -0800

    switch to pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_09_173431) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.string "title"
     t.string "address"
