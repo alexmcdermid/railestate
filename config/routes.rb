19cfeb37af02677bdf12da182000e3a5836b17a8
alexmcdermid <alexander.mcdermid@gmail.com>
2023-02-15 11:23:59 -0800

    fix styling breaking log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'sessions' }
   resources :listings
   root 'listings#index'
   post 'listings/new', to: 'listings#create'
   devise_scope :user do
-    get '/sign-in' => "devise/sessions#new", :as => :login
+    get '/sign_in' => "devise/sessions#new", :as => :login
   end
 end
