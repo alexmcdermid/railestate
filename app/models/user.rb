f20b128dd602ad584101ad0d161249b9bcbf8020
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-09-01 13:50:05 -0700

    fix env and update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         # for google oauth
+         :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :listings, dependent: :destroy
 end
