93d2084c1a082e08764fa9e26d356fea56eff4d8
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-02-25 09:22:54 -0800

    migrate

--- db/migrate/20231001191603_rename_users_to_rails_users.rb
+++ db/migrate/20231001191603_rename_users_to_rails_users.rb
@@ -2,7 +2,10 @@ class RenameUsersToRailsUsers < ActiveRecord::Migration[7.0]
   def change
     rename_table :users, :rails_users
 
-    remove_foreign_key :listings, :users
+    if foreign_key_exists?(:listings, :users)
+      remove_foreign_key :listings, :users
+    end
+
     add_foreign_key :listings, :rails_users, column: :user_id
   end
 end
