0392145cffe5a9c7f3bed681aee77bf646363c25
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-02-25 13:07:41 -0800

    api docs

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :listings, only: [:index]
     end
-  end   
+  end
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
 end
