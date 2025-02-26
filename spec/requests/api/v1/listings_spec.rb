ce074781b758f97ac0371363e19079f15e1a7009
alexmcdermid <alexander.mcdermid@alumni.ubc.ca>
2025-02-26 03:51:31 +0000

    listing_spec

--- spec/requests/api/v1/listings_spec.rb
+++ spec/requests/api/v1/listings_spec.rb
@@ -1,7 +1,21 @@
-require 'rails_helper'
+# spec/requests/api/v1/listings_spec.rb
+require 'swagger_helper'
 
-RSpec.describe "Api::V1::Listings", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Listings API', type: :request do
+  path '/api/v1/listings' do
+    get 'Retrieves all listings' do
+      tags 'Listings'
+      produces 'application/json'
+      security [ApiKeyAuth: []]
+
+      response '200', 'listings found' do
+        schema type: :array, items: { '$ref' => '#/components/schemas/Listing' }
+        run_test!
+      end
+
+      response '401', 'unauthorized' do
+        run_test!
+      end
+    end
   end
 end
